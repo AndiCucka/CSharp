d626ed135bb8b17fa7d7d3d9d69ca04a60a41a2c
AndiCucka <124384763+AndiCucka@users.noreply.github.com>
2023-07-05 06:00:41 +0000

    CSharp

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float number = 2.5f;
+            int secondNumbrt = 17;
+            float total = number + secondNumbrt;
+
+            Console.WriteLine("Altitude".ToUpper());
+
+        }
+    }
+}
